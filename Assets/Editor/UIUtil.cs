364c3a51e0d54bce1273acd1f7c3a0a37a80a9f5
daixxtech <daixxtech@163.com>
2021-01-01 14:20:19 +0800

    [added] editor UI utility;

---
+++ Assets/Editor/UIUtil.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    public static class UIUtil {
+        private static readonly StringBuilder BUILDER = new StringBuilder();
+
+        [MenuItem("Tools/CopyUITransformPath %#X")]
+        public static void CopyUITransformPath() {
+            Transform cur = Selection.activeTransform;
+            if (cur == null) {
+                return;
+            }
+            BUILDER.Clear().Insert(0, cur.name);
+            while (true) {
+                Transform parent = cur.parent;
+                if (parent == null || parent.GetComponent<Canvas>() != null) {
+                    break;
+                }
+                BUILDER.Insert(0, "/").Insert(0, parent.name);
+                cur = parent;
+            }
+            string path = BUILDER.ToString();
+            Debug.Log($"[CopyPath] {path}");
+            GUIUtility.systemCopyBuffer = path;
+        }
+    }
+}
